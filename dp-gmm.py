087c1f6aff52792f2fd5615b8328710da99645b6
John Stechschulte <john.l.stechschulte@gmail.com>
2016-05-01 22:26:23 -0600

    now with plots

--- dp-gmm.py
+++ dp-gmm.py
@@ -3,6 +3,9 @@
 from numpy.random import multivariate_normal
 from random import uniform
 import sys
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+import numpy as np
 
 if len(sys.argv) != 2:
   print("Usage: {} <n>".format(sys.argv[0]))
@@ -21,7 +24,27 @@ def find_index(draw, urn):
     i+=1
   return i-1
 
+
+def plot_cov_ellipse(mean, cov):
+  vals, vecs = np.linalg.eigh(cov)
+  order = vals.argsort()[::-1]
+  vals = vals[order]
+  vecs = vecs[:,order]
+
+  theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+
+  # Width and height are "full" widths, not radius
+  width, height = 2 * np.sqrt(np.abs(vals))
+  ellip = Ellipse(xy=mean, width=width, height=height, angle=theta, alpha=0.2)
+
+  ax = plt.gca()
+  ax.add_artist(ellip)
+
+
 alpha = 1
+
+x = []
+y = []
 
 colors = []
 urn = [alpha]
@@ -36,7 +59,17 @@ for _ in range(int(sys.argv[1])):
     urn[i] = 0
   urn[i] += 1
   n += 1
-  print(multivariate_normal(colors[i][0], colors[i][1]))
+  p = multivariate_normal(colors[i][0], colors[i][1])
+  x.append(p[0])
+  y.append(p[1])
+  print(p)
+
+for color in colors:
+  plt.scatter([color[0][0]], [color[0][1]], marker="*")
+  plot_cov_ellipse(*color)
 
 print(colors)
 print(urn)
+
+plt.scatter(x, y)
+plt.show()
